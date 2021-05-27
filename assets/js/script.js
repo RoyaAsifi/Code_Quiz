545f776b329f8255cf0e037689d25cd90f82d49d
Roya Asifi <royaasifi@icloud.com>
2021-05-27 16:52:33 -0400

    created questions on js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: "Commonly used data type DO NOT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c. alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within _____:",
+        choices: ["a. quotes", "b. curly brackets", "c. parenthesis", "d. square brackets"],
+        answer: "c. parenthesis"
+    },
+    {
+        question: "Arrays in a JavaScript can be used to store:",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "d. all of the above"
+    },
+    {
+        question: "String values muse be enclosed within ______ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c. quotes"
+    },
+    {
+        question: "A very useful tool during development and debugging for printing content to the debugger is:",
+        choices: ["a. JavaScript", "b. terminal/bash", "c. for loops", "d. console.log"],
+        answer: "d. console.log"
+    }
+];
